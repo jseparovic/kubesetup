61fa848a0e14e075bc36b38b12ab6a967fee58fd
Jason <jseppa01@gmail.com>
2018-11-10 13:54:36 -0800

    update

--- install-helm.sh
+++ install-helm.sh
@@ -10,9 +10,9 @@ run wget https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64
 run tar xfvz helm-v2.11.0-linux-amd64.tar.gz
 run cp linux-amd64/helm /usr/bin/
 run helm init
-run kubectl create serviceaccount --namespace kube-system tiller
-run kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-run kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+#run kubectl create serviceaccount --namespace kube-system tiller
+#run kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+#run kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 run helm list
 run helm repo update
 
